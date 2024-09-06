fccde01c005eb87803d5d8eceb341d911010086c
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2024-09-06 07:53:25 +0200

    add db adapter for distances

---
+++ pkg/adapters/db/distance_db.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"github.com/hanna3-14/BackTheMiles/pkg/domain"
+)
+
+const selectDistances string = `
+	SELECT
+	rowid,
+	name,
+	distanceInMeters
+	FROM distances
+	`
+
+func (repo *SQLDBAdapter) CreateDistance(distance domain.Distance) error {
+	const createStmt string = `
+	CREATE TABLE IF NOT EXISTS distances (
+		name TEXT,
+		distanceInMeters INT
+	);`
+
+	_, err := repo.Database.Exec(createStmt)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := repo.Database.Prepare("INSERT INTO distances (name, distanceInMeters) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(distance.Name, distance.DistanceInMeters)
+	return err
+}
+
+func (repo *SQLDBAdapter) FindAllDistances() ([]domain.Distance, error) {
+	var distances []domain.Distance
+	response, err := repo.Database.Query(selectDistances)
+	if err != nil {
+		return []domain.Distance{}, err
+	}
+
+	for response.Next() {
+		var distance domain.Distance
+		err = response.Scan(&distance.ID, &distance.Name, &distance.DistanceInMeters)
+		if err != nil {
+			return []domain.Distance{}, err
+		}
+		distances = append(distances, distance)
+	}
+	return distances, nil
+}
+
+func (repo *SQLDBAdapter) FindDistanceByID(id int) (domain.Distance, error) {
+	stmt, err := repo.Database.Prepare(selectDistances + " WHERE rowid = ?")
+	if err != nil {
+		return domain.Distance{}, err
+	}
+
+	var distance domain.Distance
+	err = stmt.QueryRow(id).Scan(&distance.ID, &distance.Name, &distance.DistanceInMeters)
+	if err != nil {
+		return domain.Distance{}, err
+	}
+	return distance, nil
+}
+
+func (repo *SQLDBAdapter) UpdateDistance(id int, distance, modifiedDistance domain.Distance) error {
+	stmt, err := repo.Database.Prepare("UPDATE distances SET name = ?, distanceInMeters = ? WHERE rowid = ?")
+	if err != nil {
+		return err
+	}
+
+	if len(modifiedDistance.Name) == 0 {
+		modifiedDistance.Name = distance.Name
+	}
+	if modifiedDistance.DistanceInMeters == 0 {
+		modifiedDistance.DistanceInMeters = distance.DistanceInMeters
+	}
+
+	_, err = stmt.Exec(distance.Name, distance.DistanceInMeters, distance.ID)
+	return err
+}
+
+func (repo *SQLDBAdapter) DeleteDistance(id int) error {
+	stmt, err := repo.Database.Prepare("DELETE FROM distances WHERE rowid = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(id)
+	return err
+}
---
+++ pkg/adapters/db/distance_db_test.go
@@ -0,0 +1,244 @@
+package db_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/hanna3-14/BackTheMiles/pkg/adapters/db"
+	"github.com/hanna3-14/BackTheMiles/pkg/domain"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateDistance(t *testing.T) {
+	tests := []struct {
+		name        string
+		distance    domain.Distance
+		expectedErr error
+	}{
+		{
+			name:        "database is closed",
+			expectedErr: sql.ErrConnDone,
+		},
+		{
+			name: "happy path",
+			distance: domain.Distance{
+				Name:             "Marathon",
+				DistanceInMeters: 42195,
+			},
+		},
+	}
+
+	database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.Nil(t, err)
+	defer database.Close()
+
+	mockDBAdapter := db.SQLDBAdapter{Database: database}
+
+	for _, tt := range tests {
+		if tt.expectedErr != nil {
+			mock.ExpectExec(`CREATE TABLE IF NOT EXISTS distances ( name TEXT, distanceInMeters INT );`).
+				WithoutArgs().
+				WillReturnError(tt.expectedErr)
+		} else {
+			mock.ExpectExec(`CREATE TABLE IF NOT EXISTS distances ( name TEXT, distanceInMeters INT );`).
+				WithoutArgs().
+				WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectPrepare(`INSERT INTO distances (name, distanceInMeters) VALUES (?, ?)`)
+			mock.ExpectExec(`INSERT INTO distances (name, distanceInMeters) VALUES (?, ?)`).
+				WithArgs(tt.distance.Name, tt.distance.DistanceInMeters).
+				WillReturnResult(sqlmock.NewResult(1, 1))
+		}
+
+		err = mockDBAdapter.CreateDistance(tt.distance)
+		if tt.expectedErr != nil {
+			require.NotNil(t, err)
+			assert.Equal(t, tt.expectedErr, err)
+		} else {
+			require.Nil(t, err)
+		}
+	}
+}
+
+func TestFindAllDistances(t *testing.T) {
+	tests := []struct {
+		name              string
+		expectedDistances []domain.Distance
+		expectedErr       error
+	}{
+		{
+			name:              "no rows in database",
+			expectedErr:       sql.ErrNoRows,
+			expectedDistances: []domain.Distance{},
+		},
+		{
+			name: "happy path",
+			expectedDistances: []domain.Distance{
+				{
+					ID:               0,
+					Name:             "Marathon",
+					DistanceInMeters: 42195,
+				},
+				{
+					ID:               1,
+					Name:             "Half marathon",
+					DistanceInMeters: 21098,
+				},
+			},
+		},
+	}
+
+	database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.Nil(t, err)
+	defer database.Close()
+
+	mockDBAdapter := db.SQLDBAdapter{Database: database}
+
+	for _, tt := range tests {
+		if tt.expectedErr != nil {
+			mock.ExpectQuery(`SELECT rowid, name, distanceInMeters FROM distances`).
+				WillReturnError(tt.expectedErr)
+		} else {
+			mock.ExpectQuery(`SELECT rowid, name, distanceInMeters FROM distances`).
+				WillReturnRows(sqlmock.NewRows([]string{"rowid", "name", "distanceInMeters"}).
+					AddRow(0, "Marathon", 42195).
+					AddRow(1, "Half marathon", 21098))
+		}
+
+		actualDistances, err := mockDBAdapter.FindAllDistances()
+		assert.Equal(t, tt.expectedDistances, actualDistances)
+		assert.Equal(t, tt.expectedErr, err)
+	}
+}
+
+func TestFindDistanceByID(t *testing.T) {
+	tests := []struct {
+		name             string
+		DistanceID       int
+		expectedDistance domain.Distance
+		expectedErr      error
+	}{
+		{
+			name:             "no rows in database",
+			DistanceID:       0,
+			expectedErr:      sql.ErrNoRows,
+			expectedDistance: domain.Distance{},
+		},
+		{
+			name:       "happy path",
+			DistanceID: 0,
+			expectedDistance: domain.Distance{
+				ID:               0,
+				Name:             "Marathon",
+				DistanceInMeters: 42195,
+			},
+		},
+	}
+
+	database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.Nil(t, err)
+	defer database.Close()
+
+	mockDBAdapter := db.SQLDBAdapter{Database: database}
+
+	for _, tt := range tests {
+		mock.ExpectPrepare("SELECT rowid, name, distanceInMeters FROM distances WHERE rowid = ?")
+		if tt.expectedErr != nil {
+			mock.ExpectQuery(`SELECT rowid, name, distanceInMeters FROM distances WHERE rowid = ?`).
+				WithArgs(tt.DistanceID).WillReturnError(tt.expectedErr)
+		} else {
+			mock.ExpectQuery(`SELECT rowid, name, distanceInMeters FROM distances WHERE rowid = ?`).
+				WithArgs(tt.DistanceID).WillReturnRows(sqlmock.NewRows([]string{"rowid", "name", "distanceInMeters"}).
+				AddRow(0, "Marathon", 42195).
+				AddRow(1, "Half marathon", 21098))
+		}
+
+		actualDistance, err := mockDBAdapter.FindDistanceByID(tt.DistanceID)
+		assert.Equal(t, tt.expectedDistance, actualDistance)
+		assert.Equal(t, tt.expectedErr, err)
+	}
+}
+
+func TestUpdateDistance(t *testing.T) {
+	tests := []struct {
+		name             string
+		storedDistance   domain.Distance
+		modifiedDistance domain.Distance
+	}{
+		{
+			name: "happy path - update name",
+			storedDistance: domain.Distance{
+				ID:               1,
+				Name:             "Marathon",
+				DistanceInMeters: 21098,
+			},
+			modifiedDistance: domain.Distance{
+				Name: "Half marathon",
+			},
+		},
+		{
+			name: "happy path - update distance",
+			storedDistance: domain.Distance{
+				ID:               1,
+				Name:             "Marathon",
+				DistanceInMeters: 3,
+			},
+			modifiedDistance: domain.Distance{
+				DistanceInMeters: 42195,
+			},
+		},
+	}
+
+	database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.Nil(t, err)
+	defer database.Close()
+
+	mockDBAdapter := db.SQLDBAdapter{Database: database}
+
+	for _, tt := range tests {
+		mock.ExpectPrepare("UPDATE distances SET name = ?, distanceInMeters = ? WHERE rowid = ?")
+		mock.ExpectExec(`UPDATE distances SET name = ?, distanceInMeters = ? WHERE rowid = ?`).
+			WithArgs(tt.storedDistance.Name, tt.storedDistance.DistanceInMeters, tt.storedDistance.ID).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err := mockDBAdapter.UpdateDistance(tt.storedDistance.ID, tt.storedDistance, tt.modifiedDistance)
+		assert.Nil(t, err)
+	}
+}
+
+func TestDeleteDistance(t *testing.T) {
+	tests := []struct {
+		name        string
+		distanceID  int
+		expectedErr error
+	}{
+		{
+			name:        "database closed",
+			expectedErr: sql.ErrConnDone,
+		},
+		{
+			name:       "happy path",
+			distanceID: 3,
+		},
+	}
+
+	database, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.Nil(t, err)
+	defer database.Close()
+
+	mockDBAdapter := db.SQLDBAdapter{Database: database}
+
+	for _, tt := range tests {
+		mock.ExpectPrepare(`DELETE FROM distances WHERE rowid = ?`).
+			WillReturnError(tt.expectedErr)
+		if tt.expectedErr == nil {
+			mock.ExpectExec(`DELETE FROM distances WHERE rowid = ?`).
+				WithArgs(tt.distanceID).
+				WillReturnResult(sqlmock.NewResult(1, 1))
+		}
+
+		err := mockDBAdapter.DeleteDistance(tt.distanceID)
+		assert.Equal(t, tt.expectedErr, err)
+	}
+}
